41713dbda24b8972b12408771342951046f10020
Dylan <dylan.reeder5@gmail.com>
2023-08-02 13:09:51 -0400

    asr

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import ytdl from 'ytdl-core';
 import fs from 'fs';
 import cp from 'child_process';
 import {createClient} from '@supabase/supabase-js';
+import {HfInference} from '@huggingface/inference';
+import fetch from 'node-fetch';
+
+// @ts-ignore
+global.fetch = fetch;
 
 dotenv.config();
 
@@ -179,6 +184,54 @@ app.post('/api/video', async (req, res) => {
       // send message when done
       res.json({message: 'done'});
     });
+});
+app.post('/api/asr', async (req, res) => {
+  const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
+  const {videoId, token} = req.body;
+  const supabase = await supabaseClient(token);
+
+  // get list of files in the videoId folder
+  const {data: list, error: listError} = await supabase.storage
+    .from('chunks')
+    .list(`chunks/${videoId}`);
+
+  if (listError) throw listError;
+
+  let fullTranscription = '';
+
+  for (const file of list) {
+    // download the file
+    const {data: downloadData, error: downloadError} = await supabase.storage
+      .from('chunks')
+      .download(`chunks/${videoId}/${file.name}`);
+
+    if (downloadError) throw downloadError;
+
+    // convert Blob to ArrayBuffer
+    const arrayBuffer = await downloadData.arrayBuffer();
+
+    try {
+      const text = await Hf.automaticSpeechRecognition({
+        model: 'jonatasgrosman/wav2vec2-large-xlsr-53-english',
+        data: arrayBuffer,
+      });
+
+      fullTranscription += text.text + '\n';
+      console.log('Transcription: ', text.text);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // upload full transcription to Supabase
+  const {error: uploadError} = await supabase.storage
+    .from('transcriptions')
+    .upload(`${videoId}/transcription.txt`, Buffer.from(fullTranscription));
+
+  if (uploadError) throw uploadError;
+
+  // send message when done
+  res.json({message: 'done'});
 });
 
 app.listen(port, () => {
