8a2dc9525809eb864f7c698e8bed483f442cdfe2
Dylan <dylan.reeder5@gmail.com>
2023-08-02 12:03:08 -0400

    dumbass ts/js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import ytdl from 'ytdl-core';
-import {supabaseClient} from './lib/supabase';
 import fs from 'fs';
 import cp from 'child_process';
+
+import {createClient} from '@supabase/supabase-js';
+
+export const supabaseClient = async (supabaseAccessToken: string) => {
+  const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+  const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+  const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+    auth: {persistSession: false},
+    global: {headers: {Authorization: `Bearer ${supabaseAccessToken}`}},
+  });
+  // set Supabase JWT on the client object,
+  // so it is sent up with all Supabase requests
+  return supabase;
+};
 
 dotenv.config();
 
